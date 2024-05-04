c033ecaf294dc952eef0a48f8d69429b486dd748
AR Assignments <abdurrehman905623@gmail.com>
2024-05-04 17:50:21 +0500

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,4 +37,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.airbnb.android:lottie:3.4.0")
 }
--- app/src/main/java/com/example/audibook/Screens/SplashScreenActivity.java
+++ app/src/main/java/com/example/audibook/Screens/SplashScreenActivity.java
@@ -1,6 +1,5 @@
-package com.example.audibook;
+package com.example.audibook.Screens;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,19 +8,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.audibook.Screens.SplashScreenActivity;
+import com.example.audibook.R;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(MainActivity.this, SplashScreenActivity.class);
-        startActivity(intent);
-        finish();
-
+        setContentView(R.layout.activity_splash_screen);
     }
 }
