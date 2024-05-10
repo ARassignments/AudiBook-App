1e9c94ee37ee1583dd45a69f4e68832cac6e4fed
AR Assignments <abdurrehman905623@gmail.com>
2024-05-10 16:20:11 +0500

    push

--- app/src/main/java/com/example/audibook/Screens/SeeMoreActivity.java
+++ app/src/main/java/com/example/audibook/Screens/SeeMoreActivity.java
@@ -1,8 +1,6 @@
 package com.example.audibook.Screens;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
@@ -13,20 +11,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.audibook.R;
 
-public class DashboardActivity extends AppCompatActivity {
+public class SeeMoreActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_see_more);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        findViewById(R.id.seeMoreBtn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(DashboardActivity.this, SeeMoreActivity.class));
-            }
-        });
     }
 }
