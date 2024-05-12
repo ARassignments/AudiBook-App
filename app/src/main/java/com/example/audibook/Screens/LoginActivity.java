6194fcbf0fe387dd7309e905e5537ff3f2e86f2e
AnsariFarah123 <168174857+AnsariFarah123@users.noreply.github.com>
2024-05-12 05:08:13 +0500

    push

--- app/src/main/java/com/example/audibook/Screens/LoginActivity.java
+++ app/src/main/java/com/example/audibook/Screens/LoginActivity.java
@@ -24,7 +24,7 @@ public class LoginActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         findViewById(R.id.registerBtn).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             }
         });
