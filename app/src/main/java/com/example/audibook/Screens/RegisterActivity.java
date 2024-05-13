1f7eada74bd23f88463a92ef7167d3de09ea5a81
AR Assignments <abdurrehman905623@gmail.com>
2024-05-13 16:32:22 +0500

    upload

--- app/src/main/java/com/example/audibook/Screens/RegisterActivity.java
+++ app/src/main/java/com/example/audibook/Screens/RegisterActivity.java
@@ -3,6 +3,8 @@ package com.example.audibook.Screens;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,8 +13,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.audibook.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText emailInput, pwdInput;
+    Button registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,33 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        emailInput = findViewById(R.id.emailInput);
+        pwdInput = findViewById(R.id.pwdInput);
+        registerBtn = findViewById(R.id.registerBtn);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(emailInput.getText().toString().equals("")){
+                    emailInput.setError("Email Address is required!!!");
+                } else {
+                    emailInput.setError(null);
+                }
+
+                if(pwdInput.getText().toString().equals("")){
+                    pwdInput.setError("Password is required!!!");
+                } else {
+                    pwdInput.setError(null);
+                }
+
+                if(!(emailInput.getText().toString().equals("")&&pwdInput.getText().toString().equals(""))){
+                    FirebaseAuth auth = FirebaseAuth.getInstance();
+                    auth.createUserWithEmailAndPassword(emailInput.getText().toString(),pwdInput.getText().toString());
+                }
+            }
+        });
+
         findViewById(R.id.cancelBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
